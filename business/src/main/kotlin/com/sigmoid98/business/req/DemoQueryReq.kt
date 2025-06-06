a8a71dea588ece8b0c73975e9d9e51b39054d218
Heylium <tuanhsiang@163.com>
2025-06-06 22:37:54 +0800

    fixed spring-boot-starter-validation annotation not work on kotlin data class issue.

--- business/src/main/kotlin/com/sigmoid98/business/req/DemoQueryReq.kt
+++ business/src/main/kotlin/com/sigmoid98/business/req/DemoQueryReq.kt
@@ -3,7 +3,10 @@ package com.sigmoid98.business.req
 import jakarta.validation.constraints.NotBlank
 
 data class DemoQueryReq(
-    // Make the field nullable even its not, This will stop kotlin validation to happen before javax
+    // A default value is required for non-nullable data class field.
+    // Make the field nullable even its not or set a default value for a non-nullable data class field,
+    // This will stop kotlin validation to happen before javax.
+    // @link https://dev.to/mikhailepatko/using-spring-boot-starter-validation-library-with-kotlin-534a
     @field:NotBlank(message = "[手机号] 不能为空")
-    val mobile: String?,
+    val mobile: String = "",
 )
