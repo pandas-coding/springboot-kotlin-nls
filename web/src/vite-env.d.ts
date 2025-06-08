5c3f61b0ae51f5d2da937dd354244a09b9076020
Heylium <tuanhsiang@163.com>
2025-06-08 23:05:29 +0800

    added vite env TS types declaration

--- web/src/vite-env.d.ts
+++ web/src/vite-env.d.ts
@@ -1,1 +1,17 @@
 /// <reference types="vite/client" />
+
+//<editor-fold desc="add vite env params types">
+interface ViteTypeOptions {
+  // 添加这行代码，你就可以将 ImportMetaEnv 的类型设为严格模式，
+  // 这样就不允许有未知的键值了。
+  strictImportMetaEnv: unknown
+}
+
+interface ImportMetaEnv {
+  readonly VITE_SERVER: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
+//</editor-fold>
