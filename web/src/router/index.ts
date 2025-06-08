55d28025d0b88d4918927a963e104acc840b1f0c
Heylium <tuanhsiang@163.com>
2025-06-09 00:05:53 +0800

    added register page.

--- web/src/router/index.ts
+++ web/src/router/index.ts
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory, type RouteRecordRaw } from "vue-router";
 import Home from "@/view/Home.vue";
 import Login from "@/view/Login.vue";
+import Register from "@/view/Register.vue";
 
 const routes: RouteRecordRaw[] = [
   { path: '/', redirect: '/login'},
   { path: '/login', component: Login},
+  { path: '/register', component: Register},
   { path: '/home', component: Home },
 ]
 
