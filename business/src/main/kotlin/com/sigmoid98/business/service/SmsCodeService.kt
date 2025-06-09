b9824313941053354da1b376d1e53f78d4da86fd
Heylium <tuanhsiang@163.com>
2025-06-09 23:37:56 +0800

    added sms code save when register interface.

---
+++ business/src/main/kotlin/com/sigmoid98/business/service/SmsCodeService.kt
@@ -0,0 +1,39 @@
+package com.sigmoid98.business.service
+
+import cn.hutool.core.util.IdUtil
+import cn.hutool.core.util.RandomUtil
+import com.baomidou.mybatisplus.extension.kotlin.KtUpdateChainWrapper
+import com.sigmoid98.business.domain.SmsCode
+import com.sigmoid98.business.enums.SmsCodeStatusEnum
+import com.sigmoid98.business.mapper.SmsCodeMapper
+import jakarta.annotation.Resource
+import org.springframework.stereotype.Service
+import java.time.LocalDateTime
+import java.util.Date
+
+@Service
+class SmsCodeService(
+    @Resource
+    val smsCodeMapper: SmsCodeMapper,
+) {
+
+    /**
+     *
+     */
+    fun sendCode(mobile: String, use: String) {
+        val now = Date()
+        val code = RandomUtil.randomNumbers(6)
+
+        // 保存短信到数据库
+        val smsCode = SmsCode().apply {
+            this.id = IdUtil.getSnowflakeNextId()
+            this.mobile = mobile
+            this.code = code
+            this.use = use
+            this.status = SmsCodeStatusEnum.NOT_USED.code
+            this.createdAt = now
+            this.updatedAt = now
+        }
+        smsCodeMapper.insert(smsCode)
+    }
+}
