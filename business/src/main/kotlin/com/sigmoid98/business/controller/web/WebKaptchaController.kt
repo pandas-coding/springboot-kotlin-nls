72fa9286f678b5df4df12c4121a3fd5eecc8420b
Heylium <tuanhsiang@163.com>
2025-06-14 01:03:54 +0800

    added kaptcha background module.

--- business/src/main/kotlin/com/sigmoid98/business/controller/web/WebKaptchaController.kt
+++ business/src/main/kotlin/com/sigmoid98/business/controller/web/WebKaptchaController.kt
@@ -4,8 +4,6 @@ import com.google.code.kaptcha.impl.DefaultKaptcha
 import io.github.oshai.kotlinlogging.KotlinLogging
 import jakarta.annotation.Resource
 import jakarta.servlet.http.HttpServletResponse
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.data.redis.core.StringRedisTemplate
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
