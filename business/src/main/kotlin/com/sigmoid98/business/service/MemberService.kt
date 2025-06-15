a7eb5ece3da2c4d317849e46e1dd8f3c5d411402
Heylium <tuanhsiang@163.com>
2025-06-15 23:49:10 +0800

    added user login service.

--- business/src/main/kotlin/com/sigmoid98/business/service/MemberService.kt
+++ business/src/main/kotlin/com/sigmoid98/business/service/MemberService.kt
@@ -6,7 +6,9 @@ import com.sigmoid98.business.domain.Member
 import com.sigmoid98.business.exception.BusinessException
 import com.sigmoid98.business.exception.BusinessExceptionEnum
 import com.sigmoid98.business.mapper.MemberMapper
+import com.sigmoid98.business.req.MemberLoginReq
 import com.sigmoid98.business.req.MemberRegisterReq
+import com.sigmoid98.business.resp.MemberLoginResp
 import io.github.oshai.kotlinlogging.KotlinLogging
 import jakarta.annotation.Resource
 import org.springframework.stereotype.Service
@@ -54,4 +56,29 @@ class MemberService(
         memberMapper.insert(registerMember)
     }
 
+    /**
+     * 登录
+     */
+    fun login(req: MemberLoginReq): MemberLoginResp {
+        val loginMobile = req.mobile
+        val savedMember = selectByMobile(loginMobile)
+        if (savedMember == null) {
+            logger.warn { "手机号不存在: $loginMobile" }
+            throw BusinessException(BusinessExceptionEnum.MEMBER_LOGIN_ERROR)
+        }
+
+        if (savedMember.password.equals(req.password, ignoreCase = true)) {
+            logger.warn { "密码错误: $loginMobile" }
+            throw BusinessException(BusinessExceptionEnum.MEMBER_LOGIN_ERROR)
+        }
+
+        logger.info { "登录成功, 登录手机号: $loginMobile" }
+        val loginResp = MemberLoginResp(
+            id = savedMember.id!!,
+            name = savedMember.name!!,
+            token = "",
+        )
+
+        return loginResp
+    }
 }
