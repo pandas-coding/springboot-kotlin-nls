49a5bfcf1885f6a4426d956e76886da40db1a9d9
Heylium <tuanhsiang@163.com>
2025-06-22 00:37:15 +0800

    added upload video button

--- web/src/view/home/FileTransfer.vue
+++ web/src/view/home/FileTransfer.vue
@@ -3,12 +3,20 @@
 </script>
 
 <template>
+
   <a-alert :message="null" type="info">
     <template #description>
       <h6>1. 上传音频，生成srt字幕文件、txt文本文件，支持多国语言</h6>
       <h6>2. <b>用户上传的音频将在10天后系统自动删除</b></h6>
     </template>
   </a-alert>
+  <br/>
+  <p>
+    <a-space>
+      <a-button type="primary">开始上传音频</a-button>
+      <a-button type="default">刷新列表</a-button>
+    </a-space>
+  </p>
 
 </template>
 
