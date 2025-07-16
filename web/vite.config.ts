328ae596dd1e347690c0e2da7efdd384b783669f
Heylium <tuanhsiang@163.com>
2025-07-17 00:39:35 +0800

    added web module support for vue-tsx.

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,25 +1,27 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import Components from 'unplugin-vue-components/vite';
-import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers';
-import path from 'node:path';
+import Components from 'unplugin-vue-components/vite'
+import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers'
+import path from 'node:path'
+import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [
-        vue(),
-        Components({
-            resolvers: [
-                AntDesignVueResolver({
-                    importStyle: false,
-                }),
-            ],
-            dts: 'src/components.d.ts',
+  plugins: [
+    vue(),
+    vueJsx(),
+    Components({
+      resolvers: [
+        AntDesignVueResolver({
+          importStyle: false,
         }),
-    ],
-    resolve: {
-        alias: {
-            '@': path.join(__dirname, './src/'),
-        }
-    }
+      ],
+      dts: 'src/components.d.ts',
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': path.join(__dirname, './src/'),
+    },
+  },
 })
