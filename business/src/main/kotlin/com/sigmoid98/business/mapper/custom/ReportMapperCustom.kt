1fea00772e7849bc5ce17a7fcaed93889b411dd0
Heylium <tuanhsiang@163.com>
2025-08-05 00:23:52 +0800

    added query statistic for latest 30 days.

--- business/src/main/kotlin/com/sigmoid98/business/mapper/custom/ReportMapperCustom.kt
+++ business/src/main/kotlin/com/sigmoid98/business/mapper/custom/ReportMapperCustom.kt
@@ -1,5 +1,6 @@
 package com.sigmoid98.business.mapper.custom
 
+import com.sigmoid98.business.resp.StatisticDateResp
 import java.math.BigDecimal
 
 interface ReportMapperCustom {
@@ -16,5 +17,14 @@ interface ReportMapperCustom {
 
     fun queryOrderAmount(): BigDecimal?
 
+    fun queryRegisterCountIn30Days(): List<StatisticDateResp>
+
+    fun queryFileTransferCountIn30Days(): List<StatisticDateResp>
+
+    fun queryFileTransferSecondIn30Days(): List<StatisticDateResp>
+
+    fun queryOrderCountIn30Days(): List<StatisticDateResp>
+
+    fun queryOrderAmountIn30Days(): List<StatisticDateResp>
 
 }
