99447c600bf0a8373e8728baf227f587af7993f9
Heylium <tuanhsiang@163.com>
2025-08-04 23:44:38 +0800

    added other basic statistic queries.

--- business/src/main/kotlin/com/sigmoid98/business/resp/StatisticResp.kt
+++ business/src/main/kotlin/com/sigmoid98/business/resp/StatisticResp.kt
@@ -1,5 +1,40 @@
 package com.sigmoid98.business.resp
 
+import java.math.BigDecimal
+
 data class StatisticResp(
-    val onLineCount: Int
+    /**
+     * 实时在线
+     */
+    val onLineCount: Int?,
+
+    /**
+     * 注册人数
+     */
+    val registerCount: Int?,
+
+    /**
+     * 语音识别数
+     */
+    val fileTransferCount: Int?,
+
+    /**
+     * 语音识别时长(秒)
+     */
+    val fileTransferSecond: Int?,
+
+    /**
+     * 订单数
+     */
+    val orderCount: Int?,
+
+    /**
+     * 订单金额
+     */
+    val orderAmount: BigDecimal?,
+
+    // /**
+    //  * 近30天注册人数
+    //  */
+    // val registerCountList: List<StatisticDateResp>,
 )
