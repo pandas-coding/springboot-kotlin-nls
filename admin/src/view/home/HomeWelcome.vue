aae405dcde1418702c2889ec0a8f366975a24fae
Heylium <tuanhsiang@163.com>
2025-08-06 23:43:23 +0800

    added frontend query statistic display in welcome page.

--- admin/src/view/home/HomeWelcome.vue
+++ admin/src/view/home/HomeWelcome.vue
@@ -1,37 +1,34 @@
 <script setup lang="ts">
-import { computed, reactive } from "vue";
-import { formatSecond } from "@/utils/tool.ts";
+import { computed, onMounted } from 'vue'
+import { formatSecond } from '@/utils/tool.ts'
+import { useQueryStatistic } from '@/view/home/useQueryStatistic.ts'
 
-const statistic = reactive({
-  onlineCount: 0,
-  registerCount: 0,
-  orderCount: 0,
-  orderAmount: 0,
-  filetransCount: 0,
-  filetransSecond: 0,
+const { statistic, queryStatistic } = useQueryStatistic()
+
+const formattedSecond = computed(() => formatSecond((statistic.value?.fileTransferSecond ?? 0).toString()))
+
+onMounted(() => {
+  queryStatistic()
 })
-
-const formattedSecond = computed(() => formatSecond(statistic.filetransSecond.toString()))
-
 </script>
 
 <template>
   <a-card>
     <a-row :gutter="[0, 20]">
       <a-col :span="4">
-        <a-statistic title="在线人数" :value="statistic.onlineCount"/>
+        <a-statistic title="在线人数" :value="statistic?.onLineCount ?? 0"/>
       </a-col>
       <a-col :span="4">
-        <a-statistic title="注册人数" :value="statistic.registerCount"/>
+        <a-statistic title="注册人数" :value="statistic?.registerCount ?? 0"/>
       </a-col>
       <a-col :span="4">
-        <a-statistic title="订单数" :value="statistic.orderCount"/>
+        <a-statistic title="订单数" :value="statistic?.orderCount ?? 0"/>
       </a-col>
       <a-col :span="4">
-        <a-statistic title="订单金额" :value="statistic.orderAmount"/>
+        <a-statistic title="订单金额" :value="statistic?.orderAmount ?? 0"/>
       </a-col>
       <a-col :span="4">
-        <a-statistic title="语音识别次数" :value="statistic.filetransCount"/>
+        <a-statistic title="语音识别次数" :value="statistic?.fileTransferCount ?? 0"/>
       </a-col>
       <a-col :span="4">
         <a-statistic title="语音识别时长" :value="formattedSecond"/>
